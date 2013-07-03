2bd547addaebb897a30c8ba9968bd95e59dcfa1e
Dan Ross <ross9885@gmail.com>
2013-07-03 00:37:05 -0700

    App sets the window title.

--- app/src/app.js
+++ app/src/app.js
@@ -1,10 +1,18 @@
-define(['game', 'canvasRenderer'], function(Game, Renderer) {
+define(['game', 'canvasRenderer', 'jquery'], function(Game, Renderer, $) {
   'use strict';
 
   var App = function(canvas) {
 
     this.game = new Game();
     this.renderer = new Renderer(canvas);
+
+  };
+
+  App.prototype = {
+
+    set title(title) {
+      document.title = title;
+    }
 
   };
 
