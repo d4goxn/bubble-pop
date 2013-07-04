9c9b23b1909deef7007b59a3c68cb29a40c24fc6
Dan Ross <ross9885@gmail.com>
2013-07-04 09:33:48 -0700

    Created an ExpressJS server.

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+
+var server = express();
+
+server.configure(function() {
+  server.use(express.static(__dirname + '/app'));
+});
+
+server.listen(3000);
